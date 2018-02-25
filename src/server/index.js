d0959472e5f774a2cde57d0f1eafa4bfddebff99
Gethyn Jones <gethyn1@gmail.com>
2018-02-25 16:57:56 +0000

    Rename server functions for consistency

--- src/server/index.js
+++ src/server/index.js
@@ -23,7 +23,7 @@ const setupRoutes = curry((config, dbs, app) => {
 
 const applyRoutes = setupRoutes(config, __, express())
 
-const listen = curry((port, isProd, app) =>
+const listenOnPort = curry((port, isProd, app) =>
   task(resolver => {
     app.listen(port, () => {
       resolver.resolve(`Server running on port ${port} ${isProd ? '(production)' : '(development)'}.`)
@@ -31,20 +31,20 @@ const listen = curry((port, isProd, app) =>
   })
 )
 
-const runApp = app =>
-  app.run().listen({
-    onRejected: logStatus,
-    onResolved: logStatus,
-  })
-
 const runInitializeDatabases = initializeDatabases({
   userapp: config.MONGODB_URI,
 }, MongoClient).run()
+
+const runListenOnPort = appTask =>
+  appTask.run().listen({
+    onRejected: logStatus,
+    onResolved: logStatus,
+  })
 
 runInitializeDatabases
   .future()
   .mapRejected(logStatus)
   .map(applyRoutes)
   .map(applyMiddleware)
-  .map(listen(WEB_PORT, IS_PROD))
-  .map(runApp)
+  .map(listenOnPort(WEB_PORT, IS_PROD))
+  .map(runListenOnPort)
